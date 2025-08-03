bb3494deb7eac1d598b65d407a3c50b3c19fe8ed
remilatoufik1-byte <remilatoufik1@gmail.com>
2025-08-03 10:33:00 +0100

    Delete script.js
    Delete script.js
    
    Signed-off-by: remilatoufik1-byte <remilatoufik1@gmail.com>

--- script.js
+++
@@ -1,5 +0,0 @@
-message.innerTextmessage.style.colorfunction generateVideo() {
-  const message = document.getElementById("message");
-  message.innerText = "⚡ Dark video is being generated... Stay tuned!";
-  message.style.color = "#ff004f";
-}
